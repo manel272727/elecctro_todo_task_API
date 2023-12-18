54104b33d82e44724f24798943e53b480100b00b
neco2021 <77984919+neco2021@users.noreply.github.com>
2023-12-18 08:56:45 +0100

    swagger implementation

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Hapi = require('@hapi/hapi');
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const Inert = require('@hapi/inert');
 const Vision = require('@hapi/vision');
 const HapiSwagger = require('hapi-swagger');
 const { Client } = require('pg');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 
 
@@ -34,202 +36,306 @@ const init = async () => {
   });
 
 
-  const swaggerOptions = {
-    info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-    },
-  };
-
-  await server.register([
-    Inert,
-    Vision,
-    {
-      plugin: HapiSwagger,
-      options: swaggerOptions,
-    },
-  ]);
 
 
 
+// GET TODOS INFO - GET REQUEST
   server.route({
     method: 'GET',
     path: '/api/data',
-    handler: async (request, h) => {
-      try {
-        const { rows } = await client.query('SELECT * FROM todos');
-        return rows;
-      } catch (err) {
-        return h.response('Error fetching data').code(500);
-      }
+    options: {
+      tags: ['api'], 
+      description: 'Get data from todos table',
+      handler: async (request, h) => {
+        try {
+          const { rows } = await client.query('SELECT * FROM todos');
+          return rows;
+        } catch (err) {
+          return h.response('Error fetching data').code(500);
+        }
+      },
     },
   });
+  
 
+// GET USERS INFO - GET REQUEST
   server.route({
     method: 'GET',
     path: '/api/userData',
-    handler: async (request, h) => {
-      try {
-        const { rows } = await client.query('SELECT * FROM authentication');
-        return rows;
-      } catch (err) {
-        return h.response('Error fetching data').code(500);
-      }
+    options: {
+      tags: ['api'],
+      description: 'Fetch user data from the authentication table',
+      handler: async (request, h) => {
+        try {
+          const { rows } = await client.query('SELECT * FROM authentication');
+          return rows;
+        } catch (err) {
+          console.error('Error fetching user data:', err);
+          return h.response('Error fetching data').code(500);
+        }
+      },
     },
   });
+  
+  
 
 
-  server.route({
-    method: 'POST',
-    path: '/api/todos',
-    handler: async (request, h) => {
-      try {
-        const { payload } = request;
-        const { todo_body } = payload; 
-
-        if (!todo_body) {
-          return h.response('Todo body is missing').code(400);
-        }
-
-        const result = await client.query('INSERT INTO todos (todo_body) VALUES ($1) RETURNING *', [todo_body]);
-
-        if (result && result.rows && result.rows.length > 0) {
-          return result.rows[0]; 
-        } else {
-          return h.response('Error inserting todo').code(500);
-        }
-      } catch (err) {
-        console.error('Error inserting todo:', err);
-        return h.response('Error inserting todo').code(500);
-      }
-    },
+ //ADD NEW TODO - POST REQUEST
+  const todoSchema = Joi.object({
+    todo_body: Joi.string().required().description('The body of the todo'),
   });
-
-
-  server.route({
-    method: 'DELETE',
-    path: '/api/todos/{id}', 
-    handler: async (request, h) => {
-      try {
-        const { id } = request.params;
-
-        const query = await client.query('DELETE FROM todos WHERE id = $1 RETURNING *', [id]);
-
-        if (query && query.rows && query.rows.length > 0) {
-          return query.rows[0]; // Return the deleted row
-        } else {
-          return h.response('Todo with specified ID not found').code(404);
-        }
-      } catch (err) {
-        console.error('Error deleting todo:', err);
-        return h.response('Error deleting todo').code(500);
-      }
-    },
-  });
-
-
-  server.route({
-    method: 'PUT',
-    path: '/api/todos/{id}', 
-    handler: async (request, h) => {
-      try {
-        const { id } = request.params;
-        const { todo_body, description } = request.payload;
-
-        if (!todo_body) {
-          return h.response('Todo body is missing').code(400);
-        }
-
-        const query = await client.query(
-          'UPDATE todos SET todo_body = $1, description = $2 WHERE id = $3 RETURNING *',
-          [todo_body, description, id]
-        );
-
-        if (query && query.rows && query.rows.length > 0) {
-          return query.rows[0]; // Return the updated row
-        } else {
-          return h.response('Todo with specified ID not found').code(404);
-        }
-      } catch (err) {
-        console.error('Error updating todo:', err);
-        return h.response('Error updating todo').code(500);
-      }
-    },
-  });
-
-
-  server.route({
-    method: 'PUT',
-    path: '/api/todos/{id}/complete',
-    handler: async (request, h) => {
-      try {
-        const { id } = request.params;
-        const { complete } = request.payload;
-
-        if (complete === undefined) {
-          return h.response('Complete status is missing').code(400);
-        }
-
-        const currentDate = new Date(); 
-        const query = await client.query('UPDATE todos SET complete = $1, finishedat = $2 WHERE id = $3 RETURNING *', [complete, currentDate, id]);
-
-        if (query && query.rows && query.rows.length > 0) {
-          return query.rows[0]; 
-        } else {
-          return h.response('Todo with specified ID not found').code(404);
-        }
-      } catch (err) {
-        console.error('Error updating complete status:', err);
-        return h.response('Error updating complete status').code(500);
-      }
-    },
-  });
-
-
-  const registerSchema = Joi.object({
-    username: Joi.string().alphanum().min(3).max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-  });
-    
   
   server.route({
     method: 'POST',
+    path: '/api/todos',
+    options: {
+      tags: ['api'],
+      description: 'Add a new todo to the database',
+      validate: {
+        payload: todoSchema,
+      },
+      handler: async (request, h) => {
+        try {
+          const { payload } = request;
+          const { todo_body } = payload;
+  
+          const result = await client.query('INSERT INTO todos (todo_body) VALUES ($1) RETURNING *', [todo_body]);
+  
+          if (result && result.rows && result.rows.length > 0) {
+            return result.rows[0];
+          } else {
+            return h.response('Error adding todo').code(500);
+          }
+        } catch (err) {
+          return h.response('Error adding todo').code(500);
+        }
+      },
+    },
+  });
+  
+
+// DELETE TODOS - DELETE REQUEST
+  server.route({
+    method: 'DELETE',
+    path: '/api/todos/{id}',
+    options: {
+      tags: ['api'],
+      description: 'Delete a todo from database',
+      validate: {
+        params: Joi.object({
+          id: Joi.number().integer().required().description('The ID of the todo to delete'),
+        }),
+      },
+      handler: async (request, h) => {
+        try {
+          const { id } = request.params;
+  
+          const query = await client.query('DELETE FROM todos WHERE id = $1 RETURNING *', [id]);
+  
+          if (query && query.rows && query.rows.length > 0) {
+            return query.rows[0]; 
+          } else {
+            return h.response('Todo not found').code(404);
+          }
+        } catch (err) {
+          console.error('Error deleting todo:', err);
+          return h.response('Error deleting todo').code(500);
+        }
+      },
+    },
+  });
+  
+
+// EDIT TODOS INFO - PATCH REQUEST
+  const todoUpdateSchema = Joi.object({
+    todo_body: Joi.string().required().description('The edited todo'),
+    description: Joi.string().description('The edited todo description'),
+  });
+  
+  server.route({
+    method: 'PATCH',
+    path: '/api/todos/{id}',
+    options: {
+      tags: ['api'],
+      description: 'Partially update a todo by ID',
+      validate: {
+        params: Joi.object({
+          id: Joi.number().integer().required().description('The ID of the todo to update'),
+        }),
+        payload: todoUpdateSchema,
+      }, 
+      handler: async (request, h) => {
+        try {
+          const { id } = request.params;
+          const { todo_body, description } = request.payload;
+  
+          if (!todo_body && !description) {
+            return h.response('Todo body or description is missing').code(400);
+          }
+  
+          const updateFields = [];
+          const queryParams = [];
+          if (todo_body) {
+            updateFields.push('todo_body = $1');
+            queryParams.push(todo_body);
+          }
+          if (description) {
+            updateFields.push('description = $2');
+            queryParams.push(description);
+          }
+          queryParams.push(id); 
+  
+          const updateQuery = await client.query(
+            `UPDATE todos SET ${updateFields.join(', ')} WHERE id = $${queryParams.length} RETURNING *`,
+            queryParams
+          );
+  
+          if (updateQuery && updateQuery.rows && updateQuery.rows.length > 0) {
+            return updateQuery.rows[0]; 
+          } else {
+            return h.response('Todo with specified ID not found').code(404);
+          }
+        } catch (err) {
+          console.error('Error updating todo:', err);
+          return h.response('Error updating todo').code(500);
+        }
+      },
+    },
+  });
+  
+
+
+  // TODOS COMPLETE - PATCH REQUEST
+  server.route({
+    method: 'PATCH',
+    path: '/api/todos/{id}/complete',
+    options: {
+      tags: ['api'],
+      description: 'Update the completion status of a todo by ID',
+      validate: {
+        params: Joi.object({
+          id: Joi.number().integer().required().description('The ID of the todo to update completion status'),
+        }),
+        payload: Joi.object({
+          complete: Joi.boolean().required().description('Status indicating if the todo is complete or not'),
+        }),
+      },
+      handler: async (request, h) => {
+        try {
+          const { id } = request.params;
+          const { complete } = request.payload;
+  
+          if (complete === undefined) {
+            return h.response('Complete status is missing').code(400);
+          }
+  
+          const currentDate = new Date();
+          const updateFields = ['complete = $1'];
+          const queryParams = [complete];
+  
+          if (complete) {
+            updateFields.push('finishedat = $2');
+            queryParams.push(currentDate);
+          }
+  
+          queryParams.push(id); 
+  
+          const query = await client.query(
+            `UPDATE todos SET ${updateFields.join(', ')} WHERE id = $${queryParams.length} RETURNING *`,
+            queryParams
+          );
+  
+          if (query && query.rows && query.rows.length > 0) {
+            return query.rows[0];
+          } else {
+            return h.response('Todo with specified ID not found').code(404);
+          }
+        } catch (err) {
+          console.error('Error updating complete status:', err);
+          return h.response('Error updating complete status').code(500);
+        }
+      },
+    },
+  });
+  
+
+  //ADD NEW USER, REGISTER - POST REQUEST
+  const registerSchema = Joi.object({
+    username: Joi.string().required().description('The username for registration'),
+    email: Joi.string().email().required().description('The email for registration'),
+    password: Joi.string().required().description('The password for registration'),
+  });
+
+  server.route({
+    method: 'POST',
     path: '/api/register',
-    handler: async (request, h) => {
-      try {
-        const { error, value } = registerSchema.validate(request.payload);
-
-        if (error) {
-          return h.response(error.details[0].message).code(400);
-        }
-
-        const { username, email, password } = value;
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const checkUserQuery = await client.query('SELECT * FROM authentication WHERE username = $1 OR email = $2', [username, email]);
-        if (checkUserQuery.rows.length > 0) {
-          return h.response('Username or email already exists').code(400);
-        }
-
-        const insertUserQuery = await client.query('INSERT INTO authentication (username, email, password) VALUES ($1, $2, $3) RETURNING *', [username, email, hashedPassword]);
-
-        if (insertUserQuery.rows && insertUserQuery.rows.length > 0) {
-          return h.response('User registered successfully').code(201);
-        } else {
+    options: {
+      tags: ['api'],
+      description: 'Register a new user',
+      validate: {
+        payload: registerSchema,
+      },
+      handler: async (request, h) => {
+        try {
+          const { error, value } = registerSchema.validate(request.payload);
+  
+          if (error) {
+            return h.response(error.details[0].message).code(400);
+          }
+  
+          const { username, email, password } = value;
+  
+          const hashedPassword = await bcrypt.hash(password, 10);
+  
+          const checkUserQuery = await client.query('SELECT * FROM authentication WHERE username = $1 OR email = $2', [
+            username,
+            email,
+          ]);
+  
+          if (checkUserQuery.rows.length > 0) {
+            return h.response('Username or email already exists').code(400);
+          }
+  
+          const insertUserQuery = await client.query(
+            'INSERT INTO authentication (username, email, password) VALUES ($1, $2, $3) RETURNING *',
+            [username, email, hashedPassword]
+          );
+  
+          if (insertUserQuery.rows && insertUserQuery.rows.length > 0) {
+            return h.response('User registered successfully').code(201);
+          } else {
+            return h.response('Error registering user').code(500);
+          }
+        } catch (err) {
+          console.error('Error registering user:', err);
           return h.response('Error registering user').code(500);
         }
-      } catch (err) {
-        console.error('Error registering user:', err);
-        return h.response('Error registering user').code(500);
-      }
+      },
     },
   });
     
-  server.route({
-    method: 'POST',
-    path: '/api/login',
+
+  //ENTER IN EXISTING ACCOUNT, LOGIN - POST REQUEST
+  const loginSchema = Joi.object({
+    email: Joi.string().email().required().description('The email for login'),
+    password: Joi.string().required().description('The password for login'),
+  });
+
+  const secretKey = crypto.randomBytes(32).toString('hex');
+  const JWT_SECRET_KEY = secretKey; 
+
+server.route({
+  method: 'POST',
+  path: '/api/login',
+  options: {
+    tags: ['api'],
+    description: 'Login',
+    validate: {
+      payload: Joi.object({
+        email: Joi.string().email().required().description('Email for login'),
+        password: Joi.string().required().description('Password for login'),
+      }),
+    },
     handler: async (request, h) => {
       try {
         const { email, password } = request.payload;
@@ -238,13 +344,13 @@ const init = async () => {
 
         if (userQuery.rows.length === 1) {
           const hashedPassword = userQuery.rows[0].password;
-          console.log('Hashed Password from DB:', hashedPassword);
 
           const passwordMatch = await bcrypt.compare(password, hashedPassword);
-          console.log('Password Match:', passwordMatch);
 
           if (passwordMatch) {
-            return h.response('Login successful').code(200);
+            const token = jwt.sign({ email }, JWT_SECRET_KEY, { expiresIn: '1h' }); // Token expires in 1 hour
+
+            return h.response({ token }).code(200);
           } else {
             return h.response('Invalid credentials').code(401);
           }
@@ -256,18 +362,27 @@ const init = async () => {
         return h.response('Error logging in').code(500);
       }
     },
-  });
+  },
+});
 
 
+  //DELETE ACCOUNT FROM DATABASE - DELETE REQUEST
   server.route({
-      method: 'DELETE',
-      path: '/api/deleteAccount/{id}',
+    method: 'DELETE',
+    path: '/api/deleteAccount/{id}',
+    options: {
+      tags: ['api'],
+      description: 'Delete an account by ID',
+      validate: {
+        params: Joi.object({
+          id: Joi.number().integer().required().description('ID for delete an account'),
+        }),
+      },
       handler: async (request, h) => {
         try {
-          const { id } = request.params; 
-
+          const { id } = request.params;
           const deleteQuery = await client.query('DELETE FROM authentication WHERE id = $1', [id]);
-    
+  
           if (deleteQuery.rowCount > 0) {
             return h.response('Account deleted successfully').code(200);
           } else {
@@ -278,7 +393,36 @@ const init = async () => {
           return h.response('Error deleting account').code(500);
         }
       },
+    },
   });
+  
+
+  const swaggerOptions = {
+    info: {
+      title: 'Elecctro To-Do Documentation',
+      version: '1.0.0',
+    },
+    definitions: {
+      Todo: {
+        type: 'object',
+        properties: {
+          todo_body: { type: 'string' },
+        
+        },
+        required: ['todo_body'],
+      },
+    },
+  };
+  
+  
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions,
+    },
+  ]);
     
 
     try {
@@ -289,9 +433,5 @@ const init = async () => {
     }
   };
 
-process.on('unhandledRejection', (err) => {
-  console.error('Unhandled promise rejection', err);
-  process.exit(1);
-});
 
 init();
